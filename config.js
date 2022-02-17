9961acf6be32efdd4a73533563a501735001b691
Anna Osipova <anna@osipova.fi>
2022-02-17 10:17:12 +0200

    Update title

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Anna Osipova',
-  heading: 'Full-stack Developer',
+  heading: 'Lead Engineer',
   // social
   socialLinks: [
     {
