d809f572fba344424c5d11725b8890391a3788c9
Anna Osipova <anna.osipova@smartly.io>
2022-08-11 17:34:22 +0300

    Update name

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Anna Osipova', // <title>
+  siteTitle: 'Anna Sunberg', // <title>
   manifestName: 'Identity',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -8,24 +8,24 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anna Osipova',
+  authorName: 'Anna Sunberg',
   heading: 'Lead Engineer',
   // social
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anna-osipova',
+      url: 'https://github.com/anna-sunberg',
     },
     {
       icon: 'fa-linkedin',
       name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/anna-osipova-0a2847100/',
+      url: 'https://www.linkedin.com/in/anna-sunberg/',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anna@osipova.fi',
+      url: 'mailto:anna@sunberg.fi',
     },
   ],
 };
